d87d46bb8c03dd06ff3fd8d8eeb43b056ae15615
Vilfred Sikker <vilfredsikker@gmail.com>
2021-09-08 20:37:25 +0200

    fetch expenses in useEffect

--- pleo-frontend/src/App.tsx
+++ pleo-frontend/src/App.tsx
@@ -1,27 +1,46 @@
+import { useEffect, useState } from "react"
 import styled, { ThemeProvider } from "styled-components"
 import { Button } from "./components/button"
 import { Nav } from "./components/nav"
 import { useThemeContext } from "./context/theme-context"
+import { Expense } from "./models/expense"
+import { ExpenseService, ExpensesRespone } from "./services/expense-service"
 import { getTheme } from "./theme"
 
 const MainContainer = styled.div`
 	display: flex;
 	flex-direction: column;
-	height: 100vh;
+	min-height: 100vh;
 
-	color: ${({ theme }) => theme.colors.main};
-	background-color: ${({ theme }) => theme.backgroundColors.main};
+	color: ${({ theme }) => theme.colors.primary};
+	background-color: ${({ theme }) => theme.colors.background.primary};
 `
 
 const App = () => {
 	const ThemeContext = useThemeContext()
+	const [expenses, setExpenses] = useState<Expense[]>()
+	const [totalExpenses, setTotalExpenses] = useState<number>()
+
+	useEffect(() => {
+		const load = async () => {
+			const service = new ExpenseService()
+			const { expenses, total }: ExpensesRespone = await service.getExpenses(
+				10,
+				0
+			)
+			console.log(expenses, total)
+			setExpenses(expenses)
+			setTotalExpenses(total)
+		}
+
+		load()
+	}, [])
 
 	return (
 		<ThemeProvider theme={getTheme(ThemeContext.theme)}>
 			<MainContainer>
 				<Nav />
-				<div>Test</div>
-				<Button>Test</Button>
+				<main></main>
 			</MainContainer>
 		</ThemeProvider>
 	)
